8a2edbc37d2ec70bbda2032bbda174bf140fbe28
Ivan Yanez <ivanyanez7@yahoo.com>
2021-01-25 20:00:52 +0000

    Changing code to use pearson correlation for collaborative filtering

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,76 +1,47 @@
-# Collaborative filtering
+# Collaborative filtering to get the Top-N recommended movies
 import os
 import numpy as np
 import pandas as pd
-from surprise import SVD, Reader, Dataset
-from surprise.model_selection import cross_validate
-
-
-# Get unique genres across all movies
-def getUniqueGenres(genres):
-    unique_genres = []
-    for row in genres:
-        # if there are multiple genres
-        if '|' in row:
-            genre_list = row.split('|')
-            # Add genre if not in genre list
-            for genre in genre_list:
-                if genre not in unique_genres:
-                    unique_genres.append(genre)
-
-    unique_genres = sorted(unique_genres)
-    return unique_genres
 
 
 def main():
     # Read in dataset
     movie_path = os.getcwd() + r'\ml-latest-small\movies.csv'
     ratings_path = os.getcwd() + r'\ml-latest-small\ratings.csv'
-    movie_data = pd.read_csv(movie_path, na_values='NA')
+    movies = pd.read_csv(movie_path, na_values='NA')
     ratings = pd.read_csv(ratings_path, na_values='NA')
 
-    # get unique users, movies and genres
-    users = ratings.userId.unique()
-    movies = movie_data.movieId.unique()
-    genres = getUniqueGenres(movie_data['genres'])
+    # Create year column from the title
+    movies['year'] = movies['title'].str.extract('(\(\d\d\d\d\))', expand=False)
+    movies['year'] = movies['year'].str.extract('(\d\d\d\d)', expand=False)
 
-    # get the top 20 users who have rated frequently
-    top_users = ratings.groupby('userId')['rating'].count()
-    sorted_top_users = top_users.sort_values(ascending=False)[0:15]
+    # Drop genres table from movies, timestamp from ratings
+    movies.drop(columns=['genres'], inplace=True)
+    ratings.drop(columns=['timestamp'], inplace=True)
 
-    # get the top 20 movies that have been rated frequently
-    top_movies = ratings.groupby('movieId')['rating'].count()
-    sorted_top_movies = top_movies.sort_values(ascending=False)[0:15]
+    # Remove years from movies title
+    movies['title'] = [x[:-7] for x in movies['title']]
 
-    # create user-movie matrix
-    um_matrix = ratings.join(sorted_top_users, on='userId', how='inner', rsuffix='_r')
-    um_matrix = um_matrix.join(sorted_top_movies, on='userId', how='inner', rsuffix='_r')
-    a = pd.crosstab(um_matrix.userId, um_matrix.movieId, um_matrix.rating, aggfunc=np.sum)
+    # Create a test user profile
+    test_user = [
+        {'title': 'Breakfast Club, The', 'rating': 4},
+        {'title': 'Toy Story', 'rating': 2.5},
+        {'title': 'Jumanji', 'rating': 3},
+        {'title': 'Pulp Fiction', 'rating': 4.5},
+        {'title': 'Akira', 'rating': 5}
+    ]
 
+    test_input = pd.DataFrame(test_user)
 
+    # Get the movie id's of movies in test_user
+    movie_id = movies[movies['title'].isin(test_input['title'].tolist())]
+    test_input = pd.merge(movie_id, test_input)
+
+    # Get all users who have watched the movies in test user profile and group them by users
+    users_watched = ratings[ratings['movieId'].isin(test_input['movieId'].tolist())]
+    users_subset = users_watched.groupby(['userId'])
+    print(users_subset.get_group(4))
+
+    # Find the most similar users using Pearson correlation
 
 main()
-
-'''
-ratings_dict = {'movieId': list(ratings.movieId),
-                'userId': list(ratings.userId),
-                'rating': list(ratings.rating)}
-
-df = pd.DataFrame(ratings_dict)
-
-# parse through rating files
-reader = Reader(rating_scale=(0.5, 5.0))
-
-######################
-# TEST SVD ALGORITHM #
-######################
-
-# load a dataset
-data = Dataset.load_builtin('ml-100k')
-
-# use SVD algorithm
-algorithm = SVD()
-
-# run a 5 cross-validation fold
-cross_validate(algorithm, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
-'''
