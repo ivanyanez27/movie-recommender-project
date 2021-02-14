764eddd96899f5299f9858f9cdc4773d12b8f2fa
Ivan Yanez <ivanyanez7@yahoo.com>
2021-02-14 00:17:44 +0000

    Skeleton for Django application

--- movie_recommender_app/movie_recommender_app/urls.py
+++ movie_recommender_app/movie_recommender_app/urls.py
@@ -1,4 +1,4 @@
-"""myproject URL Configuration
+"""movie_recommender_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('website/', include('website.urls')),
+    path('', RedirectView.as_view(url='website/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
