0e796e6b3722950385c02c8e5cfa9083da190bbe
Ivan Yanez <ivanyanez7@yahoo.com>
2021-02-24 17:59:32 +0000

    Replaced django model

---
+++ movierecommender/api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from .models import Movie, Rating
+from django.contrib.auth.models import User
+
+
+# Movie Serializer
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'description', 'no_of_ratings', 'avg_rating')
+
+
+# Rating Serializer
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('id', 'rating', 'user', 'movie')
+
+
+# User serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    # Create a user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        token = Token.objects.create(user=user)
+        return user
---
+++ movierecommender/api/views.py
@@ -0,0 +1,50 @@
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication,)
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            user = request.user
+
+            # if movie rating is created already
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.rating = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, rating=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            response = {'message': 'you need to give ratings'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication,)
